8828d651020d86b2c252b3e812303d7d12078708
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-08-19 07:58:53 -0300

    refactor: update to use onClick event from xml

--- app/src/main/java/com/example/calculaimc/MainActivity.java
+++ app/src/main/java/com/example/calculaimc/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -23,31 +24,43 @@ public class MainActivity extends AppCompatActivity {
 
         peso = findViewById(R.id.peso);
         altura = findViewById(R.id.altura);
-        calcular = findViewById(R.id.calcular);
         limpar = findViewById(R.id.limpar);
         resultado = findViewById(R.id.resultado);
-
-        calcular.setOnClickListener(new View.OnClickListener() {
+        limpar.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                double numPeso = Double.parseDouble(peso.getText().toString());
-                double numAltura = Double.parseDouble(altura.getText().toString());
-
-                double numImc = numPeso / Math.pow(numAltura, 2);
-
-                DecimalFormat decimalFormat = new DecimalFormat("0.00");
-
-                resultado.setText(decimalFormat.format(numImc));
+            public boolean onLongClick(View view) {
+                Toast.makeText(MainActivity.this, "Limpa os componentes", Toast.LENGTH_SHORT).show();
+                return false;
             }
         });
-        limpar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                resultado.setText("0");
-                peso.setText("");
-                altura.setText("");
-                peso.requestFocus();
-            }
-        });
+    }
+
+    public void calcularOnClick(View v) {
+        if (peso.getText().toString().equals("")) {
+            peso.setError("Campo deve ser preenchido");
+            peso.requestFocus();
+            return;
+        }
+
+        if (altura.getText().toString().equals("")) {
+            altura.setError("Campo deve ser preenchido");
+            altura.requestFocus();
+            return;
+        }
+        double numPeso = Double.parseDouble(peso.getText().toString());
+        double numAltura = Double.parseDouble(altura.getText().toString());
+
+        double numImc = numPeso / Math.pow(numAltura, 2);
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+        resultado.setText(decimalFormat.format(numImc));
+    }
+
+    public void limparOnClick(View v) {
+        resultado.setText("0");
+        peso.setText("");
+        altura.setText("");
+        peso.requestFocus();
     }
 }
